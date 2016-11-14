5a0307d19d4d7c91e847d90d9831ae2f2b023145
makeze <max.gana93@gmail.com>
2016-11-14 02:22:45 +0100

    Receiving signal strength

---
+++ SignalMap/app/src/main/java/com/example/mapdemo/SignalStrengthModule.java
@@ -0,0 +1,18 @@
+package com.example.mapdemo;
+
+import android.content.Context;
+import android.provider.Telephony;
+import android.telephony.*;
+
+/**
+ * Created by maksudg on 09/11/2016.
+ */
+
+public class SignalStrengthModule  {
+
+    public static void main(String[] args){
+
+    }
+
+
+}
