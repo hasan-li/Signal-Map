98981e21521026710140ce5e142feb7a6f717872
hasanello <gasanello@gmail.com>
2016-11-28 21:38:39 +0100

    getting decimal and mantissa of x

--- server/index.c
+++ server/index.c
@@ -2,47 +2,40 @@
 #include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 #include "data.h"
 
-
-//float removeSubstring(char *s,const char *toremove) {
-//    float *ret;
-//    while( s=strstr(s,toremove) ){
-//        ret = (float) memmove(s,s+strlen(toremove),1+strlen(s+strlen(toremove)));
-//    }
-//    return ret;
-//}
 
 int main (void){
 	printf("Content-type: text/html\n\n");
 	printf("CGI-Program has started <br />");
     
-    char m[200];
+//    query saved in var query
+    char query[200];
 	char *data = malloc(200);
-    data=getenv("QUERY_STRING");
-    sscanf(data,"%s", m);
-//    printf("%s", m);
-    
+    data = getenv("QUERY_STRING");
+    sscanf(data,"%s", query);
     
     int i = 0;
-    char *token = strtok(m, "&");
+    int numOfQueryVal = 3;  // number of values in query
+    
+//    getting data from query
+    char *token = strtok(query, "&");
     char *queryData[3];
     
-    
-    
-//    printf( " %s\n", token );
-    
-    
+
+//    getting values in query inside queryData
+//    * queryData[0] - x
+//    * queryData[1] - y
+//    * queryData[2] - strength
+
     while (token != NULL) {
         queryData[i++] = token;
         token = strtok (NULL, "&");
     }
     
     
-    const char ch = '=';
-    char *ret[3];
-    
-    for (i = 0; i < 2; ++i) {
+    for (i = 0; i < numOfQueryVal; ++i) {
         if(strstr(queryData[i], "=") != NULL) {
             queryData[i] = queryData[i] + 2;
         }
@@ -53,6 +46,14 @@ int main (void){
     
     
     
+//    getting decimal and mantissa of x
+    double x, y, x_mantissa, x_decimal;
+
+    x = atof(queryData[0]);
+    x_mantissa = modf(x, &x_decimal);
+
+    printf("Integral part = %lf\n", x_decimal);
+    printf("Fraction Part = %lf \n", x_mantissa);
     
     
     
